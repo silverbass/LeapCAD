52432a1d3a939a42ac120e1c1cd7b64eeda0ba79
Wenlong <wenlongx@gmail.com>
2015-04-05 04:42:14 -0700

    creation commands (c_type) for cube and torus

--- command.py
+++ command.py
@@ -16,6 +16,9 @@ def scalarMaker(c_type):
     elif (c_type == 3):
         # SCALE SCALAR
         return 1
+    elif (c_type == 4) || (c_type == 5):
+        # OBJECT CREATION
+        return 1
     else:
         print "ERROR: cmd not found %d" % c_type 
         return 0
@@ -28,6 +31,10 @@ def command(c_type, pVec):
         cmds.rotate(k*pVec[0], k*pVec[1], k*pVec[2], relative=True)
     elif c_type == 3:
         cmds.scale(k*pVec[0], k*pVec[1], k*pVec[2], relative=True)
+    elif c_type == 4:
+        cmds.polyCube(sx=8, sy=8, sz=8, w=16, h=16, d=16)
+    elif c_type == 5:
+        cmds.polyTorus(sx=32, sy=16, r=10, sr=4)
 
 command(c_type, pVec)
 mel.eval("refresh -f")
