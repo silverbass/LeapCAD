ad00476d808a6c606b65e3042d28be2a45db746a
Ray Chen <raychensg@berkeley.edu>
2015-04-05 05:49:11 -0700

    myo stuff

---
+++ myoframework.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export PYTHONPATH=$PYTHONNPATH:/Users/raychen/Documents/Design/Hackathons/LAHacks/LeapCAD/myo-python
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/raychen/Documents/Design/Hackathons/LAHacks/LeapCAD/myo-python/sdk/myo.framework
+echo $PATH
+echo $DYLD_LIBRARY_PATH
